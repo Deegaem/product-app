fb1393eacb7e9e30c61b914583e70320f205e625
elsadiq <sadig@gmx.ch>
2026-01-02 20:59:56 +0100

    changes: - implement crud operations in ProductHandler

--- src/main/java/org/deegaem/product_app/boundary/ProductHandler.java
+++ src/main/java/org/deegaem/product_app/boundary/ProductHandler.java
@@ -1,5 +1,7 @@
 package org.deegaem.product_app.boundary;
 
+import org.deegaem.product_app.domain.Product;
+import org.deegaem.product_app.repository.ProductRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -13,5 +15,39 @@ import reactor.core.publisher.Mono;
 public class ProductHandler {
     private static final Logger logger
             = LoggerFactory.getLogger(ProductHandler.class);
+    private final ProductRepository productRepository;
+
+    public ProductHandler(ProductRepository productRepository){
+        this.productRepository=productRepository;
+    }
+
+    public Mono<ServerResponse> listProducts(ServerRequest request) {
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+                .body(productRepository.findAll(), Product.class);
+    }
+
+    public Mono<ServerResponse> getProduct(ServerRequest request) {
+        String id = request.pathVariable("product_id");
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+                .body(productRepository.findById(id), Product.class);
+    }
+
+    public Mono<ServerResponse> saveProduct(ServerRequest request) {
+        return request.bodyToMono(Product.class).flatMap(data -> {
+            return ServerResponse.ok().body(productRepository.save(data), Product.class);
+        });
+    }
+
+    public Mono<ServerResponse> updateProduct(ServerRequest request) {
+        return request.bodyToMono(Product.class).flatMap(data -> {
+            return ServerResponse.ok().body(productRepository.save(data), Product.class);
+        });
+    }
+
+    public Mono<ServerResponse> deleteProduct(ServerRequest request) {
+        String id = request.pathVariable("product_id");
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+                .body(productRepository.deleteById(id), Product.class);
+    }
 }
 
