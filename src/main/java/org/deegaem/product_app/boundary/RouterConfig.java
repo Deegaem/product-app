74df7325c472b6ac2124f4bd891e2dbdd51178ab
elsadiq <sadig@gmx.ch>
2026-01-02 21:42:03 +0100

    changes: - implemented productsRouten

--- src/main/java/org/deegaem/product_app/boundary/RouterConfig.java
+++ src/main/java/org/deegaem/product_app/boundary/RouterConfig.java
@@ -1,4 +1,23 @@
 package org.deegaem.product_app.boundary;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
 public class RouterConfig {
+    @Bean
+    public RouterFunction<ServerResponse> productsRouten(ProductHandler productHandler) {
+        return RouterFunctions.route()
+                .GET("/products", productHandler::listProducts)
+                .GET("/products/{product_id}", RequestPredicates.accept(MediaType.TEXT_PLAIN), productHandler::getProduct)
+                .POST("/products", RequestPredicates.contentType(MediaType.APPLICATION_JSON), productHandler::saveProduct)
+                .PUT("/products", RequestPredicates.contentType(MediaType.APPLICATION_JSON), productHandler::updateProduct)
+                .DELETE("/products/{product_id}", RequestPredicates.accept(MediaType.TEXT_PLAIN), productHandler::deleteProduct)
+                .build();
+    }
 }
